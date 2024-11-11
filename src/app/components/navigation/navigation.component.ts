87ee16f7d7050b49b6f6b60f948c39756c78137a
Mamello <mmoshoeshoe@webelieve.co.za>
2024-11-11 20:58:07 +0200

    add angular material and navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  desktopMenu = false
+  mobileMenu = false
+
+  scrollTo(className: string):void {
+    const elementList = document.querySelectorAll('.' + className);
+    const element = elementList[0] as HTMLElement;
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
