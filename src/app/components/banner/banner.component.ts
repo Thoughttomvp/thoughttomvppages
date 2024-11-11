8c09c71c8c5453ca3a99f637107baa915875c7b5
Mamello <mmoshoeshoe@webelieve.co.za>
2024-11-11 18:59:27 +0200

    add banner

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss'
+})
+export class BannerComponent {
+
+  scrollTo(className: string):void {
+    const elementList = document.querySelectorAll('.' + className);
+    const element = elementList[0] as HTMLElement;
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+
+}
