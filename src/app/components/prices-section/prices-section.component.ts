6da792533da5990ddbe16ba4fba0186f9efdb81a
Mamello <mmoshoeshoe@webelieve.co.za>
2024-11-12 19:09:25 +0200

    add pricing section

---
+++ src/app/components/prices-section/prices-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prices-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './prices-section.component.html',
+  styleUrl: './prices-section.component.scss'
+})
+export class PricesSectionComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { BenefitsComponent } from '../../components/benefits/benefits.component';
+import { PricesSectionComponent } from '../../components/prices-section/prices-section.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     BannerComponent,
-    BenefitsComponent
+    BenefitsComponent,
+    PricesSectionComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
