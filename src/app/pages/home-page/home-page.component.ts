a0ba17d3c11ad4a783c8a2cf9fcda2f4e919b0dd
Mamello <mmoshoeshoe@webelieve.co.za>
2024-11-14 17:54:04 +0200

    update faqs section

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { BenefitsComponent } from '../../components/benefits/benefits.component';
 import { PricesSectionComponent } from '../../components/prices-section/prices-section.component';
+import { FaqSectionComponent } from '../../components/faq-section/faq-section.component';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +10,8 @@ import { PricesSectionComponent } from '../../components/prices-section/prices-s
   imports: [
     BannerComponent,
     BenefitsComponent,
-    PricesSectionComponent
+    PricesSectionComponent,
+    FaqSectionComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
